2bc917793892aeac5b268940e15cd7bf75d44073
DucLM42 <leduchien09@gmail.com>
2023-09-25 11:54:15 +0700

    React native project init

---
+++ PAHMobileClient/navigation/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {
+    Text,
+    View,
+} from 'react-native';
+
+function App(props) {
+    return <View>
+        <Text>Index</Text>
+    </View>
+}
+
+export default App;
