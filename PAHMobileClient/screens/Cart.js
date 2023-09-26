a445ff0c58de6f8b7216fce9b7a6d0c1799d22b9
DucLM42 <leduchien09@gmail.com>
2023-09-26 17:17:33 +0700

    Complete basic cart page layout

--- PAHMobileClient/screens/Cart.js
+++ PAHMobileClient/screens/Cart.js
@@ -6,6 +6,8 @@ import {
     TouchableOpacity,
     StyleSheet,
     ScrollView,
+    FlatList,
+    SafeAreaView,
 } from 'react-native';
 import { AuthContext } from '../context/AuthContext';
 import { colors, fontSizes } from '../constants';
@@ -24,31 +26,36 @@ function Cart(props) {
     // Cart data
     const [cart, setCart] = useState([
         {
+            user_id: 12,
             seller_name: 'avd seller',
-            seller_address: 'some random address',
+            seller_address: '2 Đ. Hải Triều, Bến Nghé, Quận 1, Thành phố Hồ Chí Minh',
             shipping_cost: '120,000',
             cart_items: [
                 {
                     name: 'New Basic Stussy Mens Black/White L/S Tee T Shirt Size Medium',
                     price: '553,658',
-                    url: 'https://i.ebayimg.com/images/g/SqQAAOSw9w9jYyqQ/s-l1600.jpg'
+                    url: 'https://i.ebayimg.com/images/g/SqQAAOSw9w9jYyqQ/s-l1600.jpg',
+                    quantity: 1
                 },
                 {
                     name: 'Trump shirt Wanted for President rea Mugshot DJT Tee shirt Republican party tee',
                     price: '426,097',
-                    url: 'https://i.ebayimg.com/images/g/r-YAAOSwe4Vk63Ai/s-l1600.jpg'
+                    url: 'https://i.ebayimg.com/images/g/r-YAAOSwe4Vk63Ai/s-l1600.jpg',
+                    quantity: 2
                 }
             ]
         },
         {
+            user_id: 13,
             seller_name: 'mckavlin',
-            seller_address: 'some random address 2',
+            seller_address: '720A Đ. Điện Biên Phủ, Vinhomes Tân Cảng, Bình Thạnh, Thành phố Hồ Chí Minh',
             shipping_cost: '90,000',
             cart_items: [
                 {
                     name: 'Supreme Scarface The World Is Yours T-Shirt Black XL 100% Authentic Tee',
                     price: '4,756,097',
-                    url: 'https://i.ebayimg.com/images/g/fIUAAOSwmnFk2PPY/s-l1600.jpg'
+                    url: 'https://i.ebayimg.com/images/g/fIUAAOSwmnFk2PPY/s-l1600.jpg',
+                    quantity: 1
                 }
             ]
         }
@@ -105,12 +112,176 @@ function Cart(props) {
             </View>
 
             {/* Cart list: items by seller */}
-            <View style={{
-                height:1000,
-                backgroundColor:'purple'
-            }}>
 
+            {cart.map((seller) =>
+                <View key={seller.user_id} style={{ marginBottom: 10 }}>
+                    <Text style={{
+                        color: 'black',
+                        fontFamily: 'OpenSans-Bold',
+                        fontSize: fontSizes.h3,
+                        marginHorizontal: 15
+                    }}>Người bán {seller.seller_name}</Text>
+                    <Text style={{
+                        color: colors.darkGreyText,
+                        fontFamily: 'OpenSans-Medium',
+                        fontSize: fontSizes.h5,
+                        marginHorizontal: 15,
+                        marginTop: 5
+                    }}>{seller.seller_address}</Text>
+                    <Text style={{
+                        color: 'black',
+                        fontFamily: 'OpenSans-Medium',
+                        fontSize: fontSizes.h4,
+                        marginHorizontal: 15,
+                        marginTop: 5,
+                        marginBottom: 10
+                    }}>{seller.shipping_cost} VND phí giao hàng (thông qua GHN)</Text>
+                    {seller.cart_items.map((cart_item) =>
+                        <View>
+                            <TouchableOpacity
+                                key={cart_item.name}
+                                style={{
+                                    flexDirection: 'row',
+                                    marginTop: 10
+                                }}>
+                                <Image source={{ uri: cart_item.url }}
+                                    style={{
+                                        width: 150,
+                                        height: 150,
+                                        margin: 5,
+                                        resizeMode: 'cover',
+                                        borderRadius: 20
+                                    }} />
+                                <View style={{
+                                    flex: 1
+                                }}>
+                                    <Text
+                                        numberOfLines={3}
+                                        ellipsizeMode='tail'
+                                        style={{
+                                            color: 'black',
+                                            fontFamily: 'OpenSans-Medium',
+                                            fontSize: fontSizes.h4,
+                                            marginHorizontal: 5
+                                        }}>{cart_item.name}</Text>
+                                    <Text
+                                        numberOfLines={3}
+                                        ellipsizeMode='tail'
+                                        style={{
+                                            color: 'black',
+                                            fontFamily: 'OpenSans-Bold',
+                                            fontSize: fontSizes.h3,
+                                            marginHorizontal: 5
+                                        }}>{cart_item.price} VND</Text>
+                                </View>
+                            </TouchableOpacity>
+
+                            <View style={{
+                                flexDirection: 'row',
+                                justifyContent: 'space-between',
+                                paddingHorizontal: 20,
+                                marginTop: 5,
+                                marginBottom: 10,
+                                alignItems: 'center'
+                            }}>
+                                <Text style={{
+                                    color: colors.darkGreyText,
+                                    fontFamily: 'OpenSans-Medium',
+                                    fontSize: fontSizes.h4
+                                }}>Số lượng: {cart_item.quantity}</Text>
+                                <TouchableOpacity>
+                                    <Text style={{
+                                        color: colors.primary,
+                                        fontFamily: 'OpenSans-Medium',
+                                        fontSize: fontSizes.h4
+                                    }}>Xóa khỏi giỏ hàng</Text>
+                                </TouchableOpacity>
+                            </View>
+                            <View style={{
+                                height: 1,
+                                backgroundColor: colors.darkGrey
+                            }}></View>
+                        </View>
+                    )}
+                </View>
+            )}
+
+            {/* Pricing */}
+            <View>
+                <View style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    paddingHorizontal: 20,
+                    marginTop: 5,
+                    marginBottom: 15
+                }}>
+                    <Text style={{
+                        color: colors.darkGreyText,
+                        fontFamily: 'OpenSans-Medium',
+                        fontSize: fontSizes.h4
+                    }}>Sản phẩm (4)</Text>
+                    <Text style={{
+                        color: 'black',
+                        fontFamily: 'OpenSans-Medium',
+                        fontSize: fontSizes.h4
+                    }}>2,568,700 VND</Text>
+                </View>
+                <View style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    paddingHorizontal: 20,
+                    marginBottom: 15
+                }}>
+                    <Text style={{
+                        color: colors.darkGreyText,
+                        fontFamily: 'OpenSans-Medium',
+                        fontSize: fontSizes.h4
+                    }}>Phí giao hàng</Text>
+                    <Text style={{
+                        color: 'black',
+                        fontFamily: 'OpenSans-Medium',
+                        fontSize: fontSizes.h4
+                    }}>108,700 VND</Text>
+                </View>
+                <View style={{
+                    height: 1,
+                    backgroundColor: colors.darkGrey
+                }}></View>
+                <View style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    paddingHorizontal: 20,
+                    marginTop: 15
+                }}>
+                    <Text style={{
+                        color: 'black',
+                        fontFamily: 'OpenSans-Medium',
+                        fontSize: fontSizes.h4
+                    }}>Tổng</Text>
+                    <Text style={{
+                        color: 'black',
+                        fontFamily: 'OpenSans-Bold',
+                        fontSize: fontSizes.h4
+                    }}>2,677,400 VND</Text>
+                </View>
             </View>
+
+            {/* Checkout button */}
+            <TouchableOpacity style={{
+                borderWidth: 1.2,
+                borderColor: colors.primary,
+                borderRadius: 35,
+                backgroundColor: colors.primary,
+                paddingVertical: 15,
+                margin: 20
+            }}>
+                <Text style={{
+                    fontSize: fontSizes.h3,
+                    fontFamily: 'OpenSans-Bold',
+                    color: 'white',
+                    textAlign: 'center'
+                }}>Tiến hành thanh toán</Text>
+            </TouchableOpacity>
         </ScrollView>
     </View>
 }
